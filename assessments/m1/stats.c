3276923214d551ccc6e1a368ab356e535989557d
Park <minpark93@gmail.com>
2024-08-20 23:23:26 -0400

    creating initial repository

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -9,13 +9,12 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief < main has 40 characters for testing. Functions for providing statistics of an array >
+ * 
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Seung Min Park>
+ * @date <Aug 19 2024 >
  *
  */
 
@@ -27,7 +26,15 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void print_statistics(unsigned char *arr, unsigned int size);
+void print_array(unsigned char *arr, unsigned int size);
+void find_median(unsigned char *arr, unsigned int size);
+void find_mean(unsigned char *arr, unsigned int size);
+void find_maximum(unsigned char *arr, unsigned int size);
+void find_minimum(unsigned char *arr, unsigned int size);
+void sort_array(unsigned char *arr, unsigned int size);
+
+int main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -38,6 +45,106 @@ void main() {
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  print_statistics(test, SIZE);
+  // print_array(test, SIZE);
+  // find_median(test, SIZE);
+  // find_mean(test, SIZE);
+  // find_maximum(test, SIZE);
+  // find_minimum(test, SIZE);
+  // sort_array(test, SIZE);
+
+  return 0;
 }
 
 /* Add other Implementation File Code Here */
+
+void print_statistics(unsigned char *arr, unsigned int size){
+  print_array(arr, size);
+  find_median(arr, size);
+  find_mean(arr, size);
+  find_maximum(arr, size);
+  find_minimum(arr, size);
+  sort_array(arr, size);
+}
+
+void print_array(unsigned char *arr, unsigned int size){
+  printf("Printing an array: ");
+  for(int i = 0; i < size; i++){
+    printf("%d ", arr[i]);
+  }
+}
+
+
+void find_median(unsigned char *arr, unsigned int size){
+  // sort the array
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size - 1; j++){
+      if(arr[j] > arr[j+1]){
+        unsigned char temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+
+  unsigned char median;
+  if(size % 2 == 0){
+    median = (arr[size/2] + arr[size/2 - 1]) / 2;
+  } else {
+    median = arr[size/2];
+  }
+
+  printf("Median: %d\n", median);
+}
+
+void find_mean(unsigned char *arr, unsigned int size) {
+  unsigned int sum = 0;
+  for(int i = 0; i < size; i++){
+    sum += arr[i];
+  }
+
+  printf("Mean: %d\n", sum / size);
+}
+
+void find_maximum(unsigned char *arr, unsigned int size){
+  unsigned char max = 0;
+
+  for(int i = 0; i < size; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+
+  printf("Maxmimum: %d\n", max);
+}
+
+void find_minimum(unsigned char *arr, unsigned int size){
+  unsigned char min = 255;
+
+  for (int i = 0; i < size; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    }
+  }
+
+  printf("Minimum: %d\n", min);
+}
+
+void sort_array(unsigned char *arr, unsigned int size){
+  // sorting the array from largest to smallest
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size - 1; j++){
+      // if current element is smaller, swap
+      if(arr[j] < arr[j+1]){
+        unsigned char temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+
+  printf("Sorted array: ");
+  for(int i = 0; i < size; i++){
+    printf("%d ", arr[i]);
+  }
+}
--- assessments/m1/stats.h
+++ assessments/m1/stats.h
@@ -9,13 +9,12 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h> 
+ * @brief <header file >
  *
- * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Seung Min Park>
+ * @date <Aug 19 2024 >
  *
  */
 #ifndef __STATS_H__
@@ -35,6 +34,71 @@
  *
  * @return <Add Return Informaiton here>
  */
+
+
+
+/**
+ * @brief <Prints the statistics of an array including minimum, maximum, mean, and median.>
+ * 
+ * @param <arr> <The array to find the statistics>
+ * @param <size> <The size of the array>
+*/
+
+void print_statistics(unsigned char *arr, unsigned int size);
+
+
+/**
+ * @brief <Given an array of data and a length, prints the array to the screen>
+ * 
+ * @param <arr> <The array to be printed>
+ * @param <size> <The size of the array>
+*/
+void print_array(unsigned char *arr, unsigned int size);
+
+/**
+ * @brief <Given an array of data and a length, returns the median value>
+ * 
+ * @param <arr> <The array to find the median value>
+ * @param <size> <The size of the array>
+*/
+
+void find_median(unsigned char *arr, unsigned int size);
+
+/**
+ * @brief <Given an array of data and a length, returns the mean value>
+ * 
+ * @param <arr> <The array to find the mean value>
+ * @param <size> <The size of the array>
+*/
+
+void find_mean(unsigned char *arr, unsigned int size);
+
+/**
+ * @brief <Given an array of data and a length, returns the maximum value>
+ * 
+ * @param <arr> <The array to find the maximum value>
+ * @param <size> <The size of the array>
+*/
+
+void find_maximum(unsigned char *arr, unsigned int size);
+
+/**
+ * @brief <Given an array of data and a length, returns the minimum value>
+ * 
+ * @param <arr> <The array to find the minimum value>
+ * @param <size> <The size of the array>
+*/
+
+void find_minimum(unsigned char *arr, unsigned int size);
+
+/**
+ * @brief <Given an array of data and a length, sorts the array from largest to smallest.>
+ * 
+ * @param <arr> <The array to be sorted>
+ * @param <size> <The size of the array>
+*/
+
+void sort_array(unsigned char *arr, unsigned int size);
 
 
 #endif /* __STATS_H__ */
